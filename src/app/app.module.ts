e3127cd843dd30ed8de21d5cf720f7be85b77914
Quentin Tellier <QTELLIER@3KLES.local>
2024-12-09 16:15:47 +0100

    Update to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { KlesMatDatepickerIntl, KlesMatMomentModule, KlesMaterialDatepickerModule } from 'kles-material-datepicker';
 import { MaterialModule } from './modules/material.module';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 import localeFr from '@angular/common/locales/fr';
 import { CommonModule, registerLocaleData } from '@angular/common';
@@ -16,40 +16,31 @@ import { MatMomentDateModule } from '@angular/material-moment-adapter';
 import { DatepickerI18n } from './datepickerI18n';
 registerLocaleData(localeFr);
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    CommonModule,
-    BrowserModule,
-    AppRoutingModule,
-    MaterialModule,
-    MatMomentDateModule,
-    KlesMaterialDatepickerModule,
-    KlesMatMomentModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    TranslateModule.forRoot(
-      {
-        loader: {
-          provide: TranslateLoader,
-          useFactory: (HttpLoaderFactory),
-          deps: [HttpClient]
-        }
-      }
-    )
-  ],
-  exports: [KlesMaterialDatepickerModule],
-  providers: [TranslateService, { provide: LOCALE_ID, useValue: 'fr-FR' },
-  {
-    provide: KlesMatDatepickerIntl, deps: [TranslateService],
-    useFactory: (translateService: TranslateService) => new DatepickerI18n(translateService).getDatepickerIntl()
-  }],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    exports: [KlesMaterialDatepickerModule],
+    bootstrap: [AppComponent], imports: [CommonModule,
+        BrowserModule,
+        AppRoutingModule,
+        MaterialModule,
+        MatMomentDateModule,
+        KlesMaterialDatepickerModule,
+        KlesMatMomentModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (HttpLoaderFactory),
+                deps: [HttpClient]
+            }
+        })], providers: [TranslateService, { provide: LOCALE_ID, useValue: 'fr-FR' },
+        {
+            provide: KlesMatDatepickerIntl, deps: [TranslateService],
+            useFactory: (translateService: TranslateService) => new DatepickerI18n(translateService).getDatepickerIntl()
+        }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 
 }
