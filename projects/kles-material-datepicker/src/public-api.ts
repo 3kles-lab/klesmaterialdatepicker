2dea3ac4b8f32cf27cef0221f87a7813a02b37d7
Quentin Tellier <QTELLIER@3KLES.local>
2025-01-15 17:33:58 +0100

    Remove adapters

--- projects/kles-material-datepicker/src/public-api.ts
+++ projects/kles-material-datepicker/src/public-api.ts
@@ -4,20 +4,11 @@
 
 export * from './lib/kles-material-datepicker.module';
 
-
 export * from './lib/datepicker';
 export * from './lib/adapters/date-adapter';
-export * from './lib/adapters/moment/moment-adapter.module';
-export * from './lib/adapters/moment/moment-format';
-export * from './lib/adapters/moment/moment-adapter';
-
-export * from './lib/adapters/luxon/luxon-adapter';
-export * from './lib/adapters/luxon/luxon-format';
-export * from './lib/adapters/luxon/luxon-adapter.module';
 
 export * from './lib/timepicker/timepicker.component';
 export * from './lib/timepicker/timeinput/timeinput.component';
 
 export * from './lib/datepicker-content/datepicker-content';
 export * from './lib/datepicker-intl/datepicker-intl';
-
