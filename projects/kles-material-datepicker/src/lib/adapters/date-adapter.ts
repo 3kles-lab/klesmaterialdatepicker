25895722fa28ac0aee3e9c1d7bad1defd18ae8db
QTELLIER <q.tellier@3kles-consulting.com>
2026-08-12 16:28:03 +0200

    feat: Update KlesMatDateAdapter implementations

--- projects/kles-material-datepicker/src/lib/adapters/date-adapter.ts
+++ projects/kles-material-datepicker/src/lib/adapters/date-adapter.ts
@@ -2,33 +2,25 @@ import { DateAdapter } from '@angular/material/core';
 
 
 export abstract class KlesMatDateAdapter<D> extends DateAdapter<D> {
-    abstract getHour(date: D): number;
-    abstract getMinute(date: D): number;
-    abstract getSecond(date: D): number;
-    abstract setHour(date: D, value: number): D;
-    abstract setMinute(date: D, value: number): D;
-    abstract setSecond(date: D, value: number): D;
     abstract getDay(date: D): string;
 
     isSameTime(a: D, b: D): boolean {
         if (a == null || b == null) return true;
-        return this.getHour(a) === this.getHour(b)
-            && this.getMinute(a) === this.getMinute(b)
-            && this.getSecond(a) === this.getSecond(b);
+        return this.getHours(a) === this.getHours(b)
+            && this.getMinutes(a) === this.getMinutes(b)
+            && this.getSeconds(a) === this.getSeconds(b);
     }
 
     copyTime(toDate: D, fromDate: D) {
-        toDate = this.setHour(toDate, this.getHour(fromDate));
-        toDate = this.setMinute(toDate, this.getMinute(fromDate));
-        toDate = this.setSecond(toDate, this.getSecond(fromDate));
+        toDate = this.setTime(toDate, this.getHours(fromDate), this.getMinutes(fromDate), this.getSeconds(fromDate));
     }
 
     compareDateWithTime(first: D, second: D, showSeconds?: boolean): number {
         let res = super.compareDate(first, second) ||
-            this.getHour(first) - this.getHour(second) ||
-            this.getMinute(first) - this.getMinute(second);
+            this.getHours(first) - this.getHours(second) ||
+            this.getMinutes(first) - this.getMinutes(second);
         if (showSeconds) {
-            res = res || this.getSecond(first) - this.getSecond(second);
+            res = res || this.getSeconds(first) - this.getSeconds(second);
         }
         return res;
     }
@@ -37,9 +29,7 @@ export abstract class KlesMatDateAdapter<D> extends DateAdapter<D> {
         if (!Array.isArray(defaultTime)) {
             throw Error('@Input DefaultTime should be an array');
         }
-        date = this.setHour(date, defaultTime[0] || 0);
-        date = this.setMinute(date, defaultTime[1] || 0);
-        date = this.setSecond(date, defaultTime[2] || 0);
+        date = this.setTime(date, defaultTime[0] || 0, defaultTime[1] || 0, defaultTime[2] || 0);
     }
 
 }
